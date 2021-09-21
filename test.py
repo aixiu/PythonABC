bbb96954c942c634d9127e2531dd539f2c8ec6e0
aixiu <4815563@qq.com>
2021-09-21 23:14:50 +0800

    日常更新

--- test.py
+++ test.py
@@ -1,21 +1,28 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-with open('test.txt', 'r', encoding='utf-8') as f:
-    for i in f:
-        print(i, end='')
+# with open('test.txt', 'r', encoding='utf-8') as f:
+#     for i in f:
+#         print(i, end='')
 
 
-with open('test.txt',mode='r', encoding='utf-8') as heine:
-    print(heine.readlines())
+# with open('test.txt',mode='r', encoding='utf-8') as heine:
+#     print(heine.readlines())
 
 
-with open('test.txt',mode='r', encoding='utf-8') as heine:
-    print(heine.readline())
+# with open('test.txt',mode='r', encoding='utf-8') as heine:
+#     print(heine.readline())
 
-with open('test.txt', 'a', encoding='utf-8') as f:
-    f.write('\n猜猜这是第几行？')
+# with open('test.txt', 'a', encoding='utf-8') as f:
+#     f.write('\n猜猜这是第几行？')
     
-with open('test.txt', 'r', encoding='utf-8') as f:
-    for i in f:
-        print(i, end='')
+# with open('test.txt', 'r', encoding='utf-8') as f:
+#     for i in f:
+#         print(i, end='')
+
+nb = 1
+while True:
+    nb = nb + 1
+    print(nb)
+    if nb == 100:
+        break
