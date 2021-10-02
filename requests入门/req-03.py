d249aab93fa9f55f1987b349c368c609623ebd26
aixiu <4815563@qq.com>
2021-10-03 00:37:50 +0800

    第一章完毕

--- requests入门/req-03.py
+++ requests入门/req-03.py
@@ -2,3 +2,26 @@
 # -*- coding: utf-8 -*-
 #@Author : Aixiu
 
+import requests
+
+url = "https://movie.douban.com/j/chart/top_list"
+
+# 重新封装参数
+param = {
+    "type": "24",
+    "interval_id": "100:90",
+    "action": "",
+    "start": "0",
+    "limit": "20",
+}
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36 Edg/94.0.992.37"
+}
+
+resp = requests.get(url, params=param, headers=headers)
+
+# print(resp.request.url)
+
+print(resp.json())
+resp.close()  # 关掉 resp
