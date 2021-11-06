1150ba84dea63d5ac810d64a201aafa394b20f73
aixiu <4815563@qq.com>
2021-11-06 15:25:28 +0800

    文件修改经典案例

--- python文件修改经典案例.py
+++ python文件修改经典案例.py
@@ -1,5 +1,12 @@
-import os
-import time
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#@Author : Aixiu
+
+
+import os  # 和操作系统相关的模块
+import time # 和时间相关的模块
+
+import pathlib
 
 # 案例文本原始内容
 
@@ -23,14 +30,18 @@ import time
 with open("./test.txt", mode="r", encoding="utf-8") as f1, \
     open("test_副本.txt", mode="w", encoding="utf-8") as f2:
     for line in f1:
-        line = line.strip()
-        if line.startswith("李"):
-            line = line.replace("李", "汪")
+        # 由于字符串为不可变类型，不能直接修改，此用，重新赋值给原字符串方式达到目地。
+        line = line.strip()   
+        if line.startswith("李"):   # 第一个字符  startwith
+            line = line.replace("李", "汪")  # 同理，字符串不可修改。  replace 替换
 
-        f2.write("\n".join(line))
+        f2.write(line)
+        f2.write("\n")
              
          
-
+time.sleep(2)
 # 删除源文件
 os.remove("./test.txt")
+time.sleep(2)
+# 把副本文件重命名为源文件名
 os.rename("test_副本.txt", "./test.txt")
