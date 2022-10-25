abff373e0905d055c5d4ba423873d6bef2c68462
aixiu <4815563@qq.com>
2022-10-25 22:29:01 +0800

    updata

--- 获取疫情数据/tqdm_模块.py
+++ 获取疫情数据/tqdm_模块.py
@@ -16,8 +16,14 @@ import time
     ncols: 调整进度条宽度, 默认是根据环境自动调节长度, 如果设置为0, 就没有进度条, 只有输出的信息
     unit: 描述处理项目的文字, 默认是'it', 例如: 100 it/s, 处理照片的话设置为'img' ,则为 100 img/s
     unit_scale: 自动根据国际标准进行项目处理速度单位的换算, 例如 100000 it/s >> 100k it/s
+    
+其它: https://blog.csdn.net/wxd1233/article/details/118371404
 
 """
+
+# 使用trange
+# trange(i) 是 tqdm(range(i)) 的简单写法
+
 # for i in trange(100):
 #     time.sleep(0.1)
     
